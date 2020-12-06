0e4211fdb23c2eff66d44aee74aa830c991d95d6
Verzaukeks <verzaukeks@gmail.com>
2020-12-06 12:57:09 +0100

    day 6
    Signed-off-by: Verzaukeks <verzaukeks@gmail.com>

---
+++ src/main/kotlin/y2020/d6.kt
@@ -0,0 +1,48 @@
+package y2020
+
+import y2020.inputs.Files
+
+object d6 {
+    fun a1() {
+        val input = Files[6].readText()
+        val questions = ArrayList<Char>()
+        var sum = 0
+
+        for (group in input.split("\r\n\r\n")) {
+            questions.clear()
+
+            for (person in group.split("\r\n"))
+                for (question in person.toCharArray()) {
+
+                    if (question !in questions)
+                        questions += question
+                }
+
+            sum += questions.size
+        }
+
+        println(sum)
+    }
+
+    fun a2() {
+        val input = Files[6].readText()
+        var sum = 0
+
+        for (group in input.split("\r\n\r\n")) {
+            val questions = IntArray('z' - 'a' + 1)
+            var people = 0
+
+            for (person in group.split("\r\n")) {
+                people++
+                for (question in person.toCharArray())
+                    questions[question - 'a']++
+            }
+
+            for (q in questions)
+                if (q == people)
+                    sum++
+        }
+
+        println(sum)
+    }
+}
