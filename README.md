e0e12db609fb74abbc53e495268da3357ebeee93
Verzaukeks <verzaukeks@gmail.com>
2021-12-24 14:24:20 +0100

    Day: 23 (year: 2021) (complete)

--- README.md
+++ README.md
@@ -3,28 +3,29 @@
 ### Possible Timings
 | Day | | a1 | a2 | a1+a2 |
 | ---: | :--- | ---: | ---: | ---: |
-| 1 | [Sonar Sweep](https://adventofcode.com/2021/day/1) | [1.1 ms](y2021/kotlin/Day01.kt#L8) | [1.1 ms](y2021/kotlin/Day01.kt#L19) | [2.2 ms](y2021/kotlin/Day01.kt) |
-| 2 | [Dive!](https://adventofcode.com/2021/day/2) | [1.6 ms](y2021/kotlin/Day02.kt#L8) | [1.4 ms](y2021/kotlin/Day02.kt#L26) | [3.0 ms](y2021/kotlin/Day02.kt) |
-| 3 | [Binary Diagnostic](https://adventofcode.com/2021/day/3) | [0.9 ms](y2021/kotlin/Day03.kt#L9) | [1.3 ms](y2021/kotlin/Day03.kt#L29) | [2.3 ms](y2021/kotlin/Day03.kt) |
-| 4 | [Giant Squid](https://adventofcode.com/2021/day/4) | [3.3 ms](y2021/kotlin/Day04.kt#L91) | [3.6 ms](y2021/kotlin/Day04.kt#L107) | [6.9 ms](y2021/kotlin/Day04.kt) |
-| 5 | [Hydrothermal Venture](https://adventofcode.com/2021/day/5) | [6.3 ms](y2021/kotlin/Day05.kt#L48) | [6.6 ms](y2021/kotlin/Day05.kt#L50) | [12.9 ms](y2021/kotlin/Day05.kt) |
-| 6 | [Lanternfish](https://adventofcode.com/2021/day/6) | [0.2 ms](y2021/kotlin/Day06.kt#L8) | [0.2 ms](y2021/kotlin/Day06.kt#L27) | [0.5 ms](y2021/kotlin/Day06.kt) |
-| 7 | [The Treachery of Whales](https://adventofcode.com/2021/day/7) | [1.0 ms](y2021/kotlin/Day07.kt#L9) | [0.5 ms](y2021/kotlin/Day07.kt#L20) | [1.5 ms](y2021/kotlin/Day07.kt) |
-| 8 | [Seven Segment Search](https://adventofcode.com/2021/day/8) | [1.1 ms](y2021/kotlin/Day08.kt#L9) | [5.9 ms](y2021/kotlin/Day08.kt#L23) | [7.0 ms](y2021/kotlin/Day08.kt) |
-| 9 | [Smoke Basin](https://adventofcode.com/2021/day/9) | [1.2 ms](y2021/kotlin/Day09.kt#L10) | [14.6 ms](y2021/kotlin/Day09.kt#L36) | [15.8 ms](y2021/kotlin/Day09.kt) |
-| 10 | [Syntax Scoring](https://adventofcode.com/2021/day/10) | [0.4 ms](y2021/kotlin/Day10.kt#L8) | [0.5 ms](y2021/kotlin/Day10.kt#L47) | [1.0 ms](y2021/kotlin/Day10.kt) |
-| 11 | [Dumbo Octopus](https://adventofcode.com/2021/day/11) | [1.4 ms](y2021/kotlin/Day11.kt#L16) | [5.0 ms](y2021/kotlin/Day11.kt#L68) | [6.5 ms](y2021/kotlin/Day11.kt) |
-| 12 | [Passage Pathing](https://adventofcode.com/2021/day/12) | [6.3 ms](y2021/kotlin/Day12.kt#L10) | [25.5 ms](y2021/kotlin/Day12.kt#L11) | [31.8 ms](y2021/kotlin/Day12.kt) |
-| 13 | [Transparent Origami](https://adventofcode.com/2021/day/13) | [1.9 ms](y2021/kotlin/Day13.kt#L10) | [5.2 ms](y2021/kotlin/Day13.kt#L11) | [7.2 ms](y2021/kotlin/Day13.kt) |
-| 14 | [Extended Polymerization](https://adventofcode.com/2021/day/14) | [3.5 ms](y2021/kotlin/Day14.kt#L9) | [3.1 ms](y2021/kotlin/Day14.kt#L10) | [6.6 ms](y2021/kotlin/Day14.kt) |
-| 15 | [Chiton](https://adventofcode.com/2021/day/15) | [17.4 ms](y2021/kotlin/Day15.kt#L14) | [285.8 ms](y2021/kotlin/Day15.kt#L16) | [303.2 ms](y2021/kotlin/Day15.kt) |
-| 16 | [Packet Decoder](https://adventofcode.com/2021/day/16) | [0.6 ms](y2021/kotlin/Day16.kt#L40) | [0.4 ms](y2021/kotlin/Day16.kt#L87) | [0.9 ms](y2021/kotlin/Day16.kt) |
-| 17 | [Trick Shot](https://adventofcode.com/2021/day/17) | [0.2 ms](y2021/kotlin/Day17.kt#L12) | [26.0 ms](y2021/kotlin/Day17.kt#L48) | [26.2 ms](y2021/kotlin/Day17.kt) |
-| 18 | [Snailfish](https://adventofcode.com/2021/day/18) | [8.1 ms](y2021/kotlin/Day18.kt#L82) | [33.0 ms](y2021/kotlin/Day18.kt#L178) | [41.2 ms](y2021/kotlin/Day18.kt) |
-| 19 | [Beacon Scanner](https://adventofcode.com/2021/day/19) | [10397.0 ms](y2021/kotlin/Day19.kt#L48) | [13725.7 ms](y2021/kotlin/Day19.kt#L52) | [24122.7 ms](y2021/kotlin/Day19.kt) |
-| 20 | [Trench Map](https://adventofcode.com/2021/day/20) | [5.7 ms](y2021/kotlin/Day20.kt#L36) | [37.8 ms](y2021/kotlin/Day20.kt#L37) | [43.5 ms](y2021/kotlin/Day20.kt) |
-| 21 | [Dirac Dice](https://adventofcode.com/2021/day/21) | [0.2 ms](y2021/kotlin/Day21.kt#L12) | [9.5 ms](y2021/kotlin/Day21.kt#L61) | [9.7 ms](y2021/kotlin/Day21.kt) |
-| 22 | [Reactor Reboot](https://adventofcode.com/2021/day/22) | [39.9 ms](y2021/kotlin/Day22.kt#L10) | [245.7 ms](y2021/kotlin/Day22.kt#L108) | [285.6 ms](y2021/kotlin/Day22.kt) |
+| 1 | [Sonar Sweep](https://adventofcode.com/2021/day/1) | [2.0 ms](y2021/kotlin/Day01.kt#L8) | [2.1 ms](y2021/kotlin/Day01.kt#L19) | [4.1 ms](y2021/kotlin/Day01.kt) |
+| 2 | [Dive!](https://adventofcode.com/2021/day/2) | [2.2 ms](y2021/kotlin/Day02.kt#L8) | [1.7 ms](y2021/kotlin/Day02.kt#L26) | [3.9 ms](y2021/kotlin/Day02.kt) |
+| 3 | [Binary Diagnostic](https://adventofcode.com/2021/day/3) | [1.9 ms](y2021/kotlin/Day03.kt#L9) | [1.5 ms](y2021/kotlin/Day03.kt#L29) | [3.4 ms](y2021/kotlin/Day03.kt) |
+| 4 | [Giant Squid](https://adventofcode.com/2021/day/4) | [3.3 ms](y2021/kotlin/Day04.kt#L91) | [3.2 ms](y2021/kotlin/Day04.kt#L107) | [6.5 ms](y2021/kotlin/Day04.kt) |
+| 5 | [Hydrothermal Venture](https://adventofcode.com/2021/day/5) | [9.8 ms](y2021/kotlin/Day05.kt#L48) | [9.0 ms](y2021/kotlin/Day05.kt#L50) | [18.9 ms](y2021/kotlin/Day05.kt) |
+| 6 | [Lanternfish](https://adventofcode.com/2021/day/6) | [0.3 ms](y2021/kotlin/Day06.kt#L8) | [0.3 ms](y2021/kotlin/Day06.kt#L27) | [0.6 ms](y2021/kotlin/Day06.kt) |
+| 7 | [The Treachery of Whales](https://adventofcode.com/2021/day/7) | [1.4 ms](y2021/kotlin/Day07.kt#L9) | [0.6 ms](y2021/kotlin/Day07.kt#L20) | [2.0 ms](y2021/kotlin/Day07.kt) |
+| 8 | [Seven Segment Search](https://adventofcode.com/2021/day/8) | [0.4 ms](y2021/kotlin/Day08.kt#L9) | [4.5 ms](y2021/kotlin/Day08.kt#L23) | [4.9 ms](y2021/kotlin/Day08.kt) |
+| 9 | [Smoke Basin](https://adventofcode.com/2021/day/9) | [1.9 ms](y2021/kotlin/Day09.kt#L10) | [14.7 ms](y2021/kotlin/Day09.kt#L36) | [16.7 ms](y2021/kotlin/Day09.kt) |
+| 10 | [Syntax Scoring](https://adventofcode.com/2021/day/10) | [0.6 ms](y2021/kotlin/Day10.kt#L8) | [0.6 ms](y2021/kotlin/Day10.kt#L47) | [1.2 ms](y2021/kotlin/Day10.kt) |
+| 11 | [Dumbo Octopus](https://adventofcode.com/2021/day/11) | [2.6 ms](y2021/kotlin/Day11.kt#L16) | [4.9 ms](y2021/kotlin/Day11.kt#L68) | [7.6 ms](y2021/kotlin/Day11.kt) |
+| 12 | [Passage Pathing](https://adventofcode.com/2021/day/12) | [3.8 ms](y2021/kotlin/Day12.kt#L10) | [26.6 ms](y2021/kotlin/Day12.kt#L11) | [30.5 ms](y2021/kotlin/Day12.kt) |
+| 13 | [Transparent Origami](https://adventofcode.com/2021/day/13) | [1.6 ms](y2021/kotlin/Day13.kt#L10) | [4.9 ms](y2021/kotlin/Day13.kt#L11) | [6.5 ms](y2021/kotlin/Day13.kt) |
+| 14 | [Extended Polymerization](https://adventofcode.com/2021/day/14) | [1.2 ms](y2021/kotlin/Day14.kt#L9) | [2.8 ms](y2021/kotlin/Day14.kt#L10) | [4.0 ms](y2021/kotlin/Day14.kt) |
+| 15 | [Chiton](https://adventofcode.com/2021/day/15) | [27.5 ms](y2021/kotlin/Day15.kt#L14) | [394.0 ms](y2021/kotlin/Day15.kt#L16) | [421.5 ms](y2021/kotlin/Day15.kt) |
+| 16 | [Packet Decoder](https://adventofcode.com/2021/day/16) | [0.4 ms](y2021/kotlin/Day16.kt#L40) | [0.3 ms](y2021/kotlin/Day16.kt#L87) | [0.7 ms](y2021/kotlin/Day16.kt) |
+| 17 | [Trick Shot](https://adventofcode.com/2021/day/17) | [0.2 ms](y2021/kotlin/Day17.kt#L12) | [26.7 ms](y2021/kotlin/Day17.kt#L48) | [26.9 ms](y2021/kotlin/Day17.kt) |
+| 18 | [Snailfish](https://adventofcode.com/2021/day/18) | [6.5 ms](y2021/kotlin/Day18.kt#L82) | [42.6 ms](y2021/kotlin/Day18.kt#L178) | [49.2 ms](y2021/kotlin/Day18.kt) |
+| 19 | [Beacon Scanner](https://adventofcode.com/2021/day/19) | [10053.1 ms](y2021/kotlin/Day19.kt#L51) | [9753.7 ms](y2021/kotlin/Day19.kt#L55) | [19806.8 ms](y2021/kotlin/Day19.kt) |
+| 20 | [Trench Map](https://adventofcode.com/2021/day/20) | [5.5 ms](y2021/kotlin/Day20.kt#L36) | [40.2 ms](y2021/kotlin/Day20.kt#L37) | [45.7 ms](y2021/kotlin/Day20.kt) |
+| 21 | [Dirac Dice](https://adventofcode.com/2021/day/21) | [0.2 ms](y2021/kotlin/Day21.kt#L12) | [9.6 ms](y2021/kotlin/Day21.kt#L61) | [9.7 ms](y2021/kotlin/Day21.kt) |
+| 22 | [Reactor Reboot](https://adventofcode.com/2021/day/22) | [60.2 ms](y2021/kotlin/Day22.kt#L10) | [307.8 ms](y2021/kotlin/Day22.kt#L108) | [368.0 ms](y2021/kotlin/Day22.kt) |
+| 23 | [Amphipod](https://adventofcode.com/2021/day/23) | [23.3 ms](y2021/kotlin/Day23.kt#L103) | [34.1 ms](y2021/kotlin/Day23.kt#L113) | [57.5 ms](y2021/kotlin/Day23.kt) |
 
 ## [Advent of Code 2020](https://adventofcode.com/2020/)
 
--- y2021/kotlin/Day23.kt
+++ y2021/kotlin/Day23.kt
@@ -1,14 +1,6 @@
 package y2021
 
 import general.Day
-import java.io.File
-import kotlin.collections.HashMap
-import kotlin.collections.contains
-import kotlin.collections.set
-
-fun main() {
-    Day23.a1()
-}
 
 object Day23 : Day() {
     override val name = "Amphipod"
@@ -20,7 +12,7 @@ object Day23 : Day() {
     private const val D: Byte = 4
 
     private val COST_MULTIPLICATOR = intArrayOf(1, 10, 100, 1000)
-    private val AMPHIPOD_TO_STACK = intArrayOf(0, 2, 4, 6)
+    private val AMPHIPOD_TO_STACK = intArrayOf(0, 4, 8, 12)
 
     private val TRAVEL_INDICES = arrayOf(
         arrayOf(
@@ -67,96 +59,81 @@ object Day23 : Day() {
         ),
     )
     private val TRAVEL_DISTANCES = arrayOf(
-        intArrayOf(4, 3, 6, 5, 8, 7, 10, 9),
-        intArrayOf(3, 2, 5, 4, 7, 6, 9, 8),
-        intArrayOf(3, 2, 3, 2, 5, 4, 7, 6),
-        intArrayOf(5, 4, 3, 2, 3, 2, 5, 4),
-        intArrayOf(7, 6, 5, 4, 3, 2, 3, 2),
-        intArrayOf(9, 8, 7, 6, 5, 4, 3, 2),
-        intArrayOf(10, 9, 8, 7, 6, 5, 4, 3)
+        intArrayOf( 6,  5,  4, 3,    8, 7, 6, 5,   10, 9, 8, 7,   12, 11, 10, 9),
+        intArrayOf( 5,  4,  3, 2,    7, 6, 5, 4,    9, 8, 7, 6,   11, 10,  9, 8),
+        intArrayOf( 5,  4,  3, 2,    5, 4, 3, 2,    7, 6, 5, 4,    9,  8,  7, 6),
+        intArrayOf( 7,  6,  5, 4,    5, 4, 3, 2,    5, 4, 3, 2,    7,  6,  5, 4),
+        intArrayOf( 9,  8,  7, 6,    7, 6, 5, 4,    5, 4, 3, 2,    5,  4,  3, 2),
+        intArrayOf(11, 10,  9, 8,    9, 8, 7, 6,    7, 6, 5, 4,    5,  4,  3, 2),
+        intArrayOf(12, 11, 10, 9,   10, 9, 8, 7,    8, 7, 6, 5,    6,  5,  4, 3),
     )
 
-    private operator fun Int.get(index: Int) = (this shr (index * 4) and 0b1111).toByte()
-    private operator fun Int.set(index: Int, value: Byte): Int {
+    private operator fun Long.get(index: Int) = (this shr (index * 4) and 0b1111).toByte()
+    private operator fun Long.set(index: Int, value: Byte): Long {
         var number = this
-        number = (number and ((-1) xor (0b1111 shl (index * 4))))
-        number = (number or (value.toInt() shl (index * 4)))
+        number = (number and ((-1L) xor (0b1111L shl (index * 4))))
+        number = (number or (value.toLong() shl (index * 4)))
         return number
     }
 
-    private fun Int.toHallwayString(): String {
-        var string = ""
-        for (h in 0..6)
-            string += when (get(h)) {
-                FREE -> "."
-                A -> "A"
-                B -> "B"
-                C -> "C"
-                D -> "D"
-                else -> get(h)
-            }
-        return string
-    }
-    private fun Int.toStacksString(): String {
-        var string = ""
-        for (s in 0..7) {
-            string += when (get(s)) {
-                FREE -> "."
-                A -> "A"
-                B -> "B"
-                C -> "C"
-                D -> "D"
-                else -> get(s)
-            }
-            if (s and 1 != 0) string += " "
+    private fun getStacks(): ByteArray {
+        val lines = INPUT.readLines()
+        val out = ByteArray(8)
+
+        fun Char.charToByte() = when (this) {
+            'A' -> A
+            'B' -> B
+            'C' -> C
+            'D' -> D
+            else -> throw Error("$this")
         }
-        return string
+
+        out[0] = lines[3][3].charToByte()
+        out[1] = lines[2][3].charToByte()
+        out[2] = lines[3][5].charToByte()
+        out[3] = lines[2][5].charToByte()
+        out[4] = lines[3][7].charToByte()
+        out[5] = lines[2][7].charToByte()
+        out[6] = lines[3][9].charToByte()
+        out[7] = lines[2][9].charToByte()
+
+        return out
     }
-
-    private fun toChar(number: Byte) = when (number) {
-        FREE -> ' '
-        A -> 'A'
-        B -> 'B'
-        C -> 'C'
-        D -> 'D'
-        else -> '?'
-    }
-
-    private fun toString(hallway: Int, stacks: Int) = """
-        #############
-        #${toChar(hallway[0])}${toChar(hallway[1])} ${toChar(hallway[2])} ${toChar(hallway[3])} ${toChar(hallway[4])} ${toChar(hallway[5])}${toChar(hallway[6])}#
-        ###${toChar(stacks[1])}#${toChar(stacks[3])}#${toChar(stacks[5])}#${toChar(stacks[7])}###
-          #${toChar(stacks[0])}#${toChar(stacks[2])}#${toChar(stacks[4])}#${toChar(stacks[6])}#
-          #########
-    """.trimIndent()
-
-    private var PATH_INDEX = 0
 
     override fun a1() {
+        val stacks = getStacks()
 
-        val CORRECT = 0
-            .set(0, A).set(1, A)
-            .set(2, B).set(3, B)
-            .set(4, C).set(5, C)
-            .set(6, D).set(7, D)
+        a(0L
+            .set( 0, A).set( 1, A).set( 2, stacks[0]).set( 3, stacks[1])
+            .set( 4, B).set( 5, B).set( 6, stacks[2]).set( 7, stacks[3])
+            .set( 8, C).set( 9, C).set(10, stacks[4]).set(11, stacks[5])
+            .set(12, D).set(13, D).set(14, stacks[6]).set(15, stacks[7]))
+    }
 
-        val cache = HashMap<Pair<Int, Int>, Int>()
+    override fun a2() {
+        val stacks = getStacks()
 
-        fun step(hallway: Int, stacks: Int, prevPath: Int): Int {
-            val path = ++PATH_INDEX
+        a(0L
+            .set( 0, stacks[0]).set( 1, D).set( 2, D).set( 3, stacks[1])
+            .set( 4, stacks[2]).set( 5, B).set( 6, C).set( 7, stacks[3])
+            .set( 8, stacks[4]).set( 9, A).set(10, B).set(11, stacks[5])
+            .set(12, stacks[6]).set(13, C).set(14, A).set(15, stacks[7]))
+    }
 
-//            run {
-//                val f = File("C:/Users/Verzaukeks/Downloads/aoc", "$path.txt")
-//                val p = File("C:/Users/Verzaukeks/Downloads/aoc", "$prevPath.txt")
-//
-//                f.writeText(toString(hallway, stacks))
-//                f.appendText("\n\n")
-//                f.appendText(p.readText())
-//            }
+    private fun a(stacks: Long) {
+
+        val CORRECT = 0L
+            .set( 0, A).set( 1, A).set( 2, A).set( 3, A)
+            .set( 4, B).set( 5, B).set( 6, B).set( 7, B)
+            .set( 8, C).set( 9, C).set(10, C).set(11, C)
+            .set(12, D).set(13, D).set(14, D).set(15, D)
+
+        val cache = HashMap<Pair<Long, Long>, Int>()
+
+        fun step(hallway: Long, stacks: Long): Int {
 
             // check if all amphipods are in their correct stack
-            if (stacks == CORRECT)
-                return 0
+            if (stacks == CORRECT) return 0
 
             // caching
             val state = Pair(hallway, stacks)
@@ -170,19 +147,17 @@ object Day23 : Day() {
 
                 // is stack accessible
                 val stackId = AMPHIPOD_TO_STACK[amphipod - 1]
-                if (stacks[stackId] != amphipod && stacks[stackId] != FREE) continue
-                if (stacks[stackId + 1] != amphipod && stacks[stackId + 1] != FREE) continue
+                for (index in 0..3)
+                    if (stacks[stackId + index] != amphipod && stacks[stackId + index] != FREE)
+                        continue@f
 
                 // is travel not blocked
                 for (ci in TRAVEL_INDICES[h][amphipod - 1])
                     if (hallway[ci] != FREE) continue@f
 
                 // get lowest stack index
-                var s = -1
-                if (stacks[stackId] == FREE) s = stackId
-                else if (stacks[stackId + 1] == FREE) s = stackId + 1
-
-//                println("$path: h=$h, amphipod=$amphipod, stack=$stackId, s=$s")
+                var s = stackId
+                while (stacks[s] != FREE) s += 1
 
                 // calculate cost
                 val distance = TRAVEL_DISTANCES[h][s]
@@ -191,45 +166,33 @@ object Day23 : Day() {
                 val newHallway = hallway.set(h, FREE)
                 val newStacks = stacks.set(s, amphipod)
 
-                return cost + step(newHallway, newStacks, path)
+                return cost + step(newHallway, newStacks)
             }
 
             // stack to hallway
             var minEnergy = 1_000_000_000
 
-//            for (s in 0..7) {
-            for (s_ in 0..7 step 2) {
-//                val amphipod = stacks[s]
-//                if (amphipod == FREE) continue
+            f@for (stackId in 0..15 step 4) {
 
                 // is movable from stack
-                if (stacks[s_] == FREE && stacks[s_ + 1] == FREE) continue
-                if (AMPHIPOD_TO_STACK[stacks[s_] - 1] == s_) {
-                    if (stacks[s_ + 1] == FREE) continue
-                    if (AMPHIPOD_TO_STACK[stacks[s_ + 1] - 1] == s_) continue
+                for (i in 0..3) {
+                    if (stacks[stackId + i] == FREE) continue@f
+                    if (AMPHIPOD_TO_STACK[stacks[stackId + i] - 1] != stackId) break
+                    if (i == 3) continue@f
                 }
-//                val stack = AMPHIPOD_TO_STACK[amphipod - 1]
-//                if (s and 1 == 0) {
-//                    if (stack == s) continue
-//                    if (stacks[s + 1] != FREE) continue
-//                } else {
-//                    if (stack == s - 1)
-//                        if (AMPHIPOD_TO_STACK[stacks[s - 1] - 1] == s - 1)
-//                            continue
-//                }
 
                 // get movable from stack
-                var s = -1
-                if (stacks[s_ + 1] != FREE) s = s_ + 1
-                else if (stacks[s_] != FREE) s = s_
+                var s = stackId + 3
+                while (stacks[s] == FREE) s -= 1
                 val amphipod = stacks[s]
+
 
                 // which hallway
                 f@for (h in 0..6) {
                     if (hallway[h] != FREE) continue
 
                     // is travel not blocked
-                    for (ci in TRAVEL_INDICES[h][s_ / 2])
+                    for (ci in TRAVEL_INDICES[h][stackId / 4])
                         if (hallway[ci] != FREE) continue@f
 
                     // calculate cost
@@ -239,12 +202,9 @@ object Day23 : Day() {
                     val newHallway = hallway.set(h, amphipod)
                     val newStacks = stacks.set(s, FREE)
 
-                    cost += step(newHallway, newStacks, path)
+                    cost += step(newHallway, newStacks)
                     if (cost < minEnergy)
                         minEnergy = cost
-
-                    if (path < 30_000)
-                    println("$cost  ->  path=$path, h=$h, s=$s")
                 }
             }
 
@@ -252,20 +212,6 @@ object Day23 : Day() {
             return minEnergy
         }
 
-//        println(step(0, 0
-//            .set(0, A).set(1, B)
-//            .set(2, D).set(3, C)
-//            .set(4, C).set(5, B)
-//            .set(6, A).set(7, D)
-//        , 0))
-        println(step(0, 0
-            .set(0, B).set(1, A)
-            .set(2, C).set(3, D)
-            .set(4, D).set(5, A)
-            .set(6, C).set(7, B)
-        , 0))
-    }
-
-    override fun a2() {
+        println(step(0, stacks))
     }
 }
