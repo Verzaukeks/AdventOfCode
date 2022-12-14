141d09bb2a347a7731ea960597ab488b75005868
Verzaukeks <verzaukeks@gmail.com>
2022-12-14 21:39:03 +0100

    alternative Day: 14 (year: 2022)

--- y2022/kotlin/Day14.kt
+++ y2022/kotlin/Day14.kt
@@ -1,13 +1,14 @@
 package y2022
 
 import general.Day
+import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
 object Day14 : Day() {
     override val name = "Regolith Reservoir"
 
-    private fun simulate(part2: Boolean = false) {
+    private fun genCave(): Pair<Array<CharArray>, Int> {
         // parse input
         val lines = INPUT.readLines().map {
             it.split(" -> ").map {
@@ -17,27 +18,19 @@ object Day14 : Day() {
             }
         }
 
-        // determine cave bounds
-        var lowX = 500
-        var highX = 500
+        // determine bounds
         var highY = 0
         for (line in lines)
-            for ((x, y) in line) {
-                lowX = min(lowX, x)
-                highX = max(highX, x)
+            for ((x, y) in line)
                 highY = max(highY, y)
-            }
 
-        if (part2) {
-            highY += 2
-            lowX = 500 - highY
-            highX = 500 + highY
-        }
-
-        // generate cave
-        val dx = (++highX) - (--lowX) + 1
+        highY += 2
+        val lowX = 500 - highY
+        val highX = 500 + highY
+        val dx = highX - lowX + 1
         val cave = Array(highY + 1) { CharArray(dx) { '.' } }
 
+        // place walls
         for (line in lines)
             for (i in 1 until line.size) {
                 val prev = line[i - 1]
@@ -53,44 +46,64 @@ object Day14 : Day() {
                         cave[y][x - lowX] = '#'
             }
 
-        if (part2) {
-            for (x in cave[highY].indices)
-                cave[highY][x] = '#'
-        }
+        // 'endless' floor
+        for (x in cave[highY].indices)
+            cave[highY][x] = '#'
 
-        // simulate
-        var steps = 0
-        while (true) {
-
-            var x = 500 - lowX
-            var y = 0
-
-            if (cave[y][x] != '.') {
-                println(steps)
-                return
-            }
-
-            while (true)
-                if (y < highY) {
-                    if (cave[y + 1][x    ] == '.') { y++;      continue }
-                    if (cave[y + 1][x - 1] == '.') { y++; x--; continue }
-                    if (cave[y + 1][x + 1] == '.') { y++; x++; continue }
-                    cave[y][x] = 'o'
-                    break
-                } else {
-                    println(steps)
-                    return
-                }
-
-            steps++
-        }
+        return Pair(cave, lowX)
     }
 
     override fun a1() {
-        simulate()
+        val (cave, lowX) = genCave()
+        val maxY = cave.size - 2
+
+        // return = <sand placed, abyss reached>
+        fun dfs(x: Int, y: Int): Pair<Int, Boolean> {
+            if (y == maxY) return Pair(0, true)
+            if (cave[y][x] != '.') return Pair(0, false)
+
+            val d = dfs(x, y + 1)
+            if (d.second) return Pair(d.first, true)
+            val l = dfs(x - 1, y + 1)
+            if (l.second) return Pair(d.first + l.first, true)
+            val r = dfs(x + 1, y + 1)
+            if (r.second) return Pair(d.first + l.first + r.first, true)
+
+            cave[y][x] = 'o'
+            return Pair(d.first + l.first + r.first + 1, false)
+        }
+
+        println(dfs(500 - lowX, 0).first)
     }
 
     override fun a2() {
-        simulate(true)
+        val (cave, lowX) = genCave()
+
+        fun bfs(): Int {
+            var count = 1
+            val q = LinkedList<Pair<Int, Int>>()
+
+            q.add(Pair(500 - lowX, 0))
+
+            while (q.isNotEmpty()) {
+                val (x, y) = q.pop()
+
+                for (ox in -1..1) {
+                    val xx = x + ox
+                    val yy = y + 1
+
+                    if (cave[yy][xx] == '.') {
+                        cave[yy][xx] = 'o'
+
+                        count++
+                        q.add(Pair(xx, yy))
+                    }
+                }
+            }
+
+            return count
+        }
+
+        println(bfs())
     }
 }
