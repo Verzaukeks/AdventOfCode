3cc4787cf62f6250775a0e2af0053d0a67017805
Verzaukeks <verzaukeks@gmail.com>
2023-12-20 19:25:32 +0100

    Day: 20 (year: 2023)

--- README.md
+++ README.md
@@ -20,6 +20,8 @@
 | 16 | [The Floor Will Be Lava](https://adventofcode.com/2023/day/16) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day16.hs#L52) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day16.hs#L57) |
 | 17 | [Clumsy Crucible](https://adventofcode.com/2023/day/17) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day17.hs#L59) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day17.hs#L64) |
 | 18 | [Lavaduct Lagoon](https://adventofcode.com/2023/day/18) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day18.hs#L34) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day18.hs#L39) |
+| 19 | [Aplenty](https://adventofcode.com/2023/day/19) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day19.hs#L69) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day19.hs#L76) |
+| 20 | [Pulse Propagation](https://adventofcode.com/2023/day/20) |  [<img src="https://www.haskell.org/img/favicon.ico" width="32" height="32"/>](y2023/haskell/day20.hs#L97) |  |
 
 ## [Advent of Code 2022](https://adventofcode.com/2022/)
 
---
+++ y2023/haskell/day20.hs
@@ -0,0 +1,101 @@
+import Data.List
+import Control.Monad (foldM)
+import Control.Monad.Trans.State (State, modify, execState)
+import Debug.Trace (trace)
+
+-- GHC.Utils.Misc
+split :: Char -> String -> [String]
+split c s = case rest of
+                []     -> [chunk]
+                _:rest -> chunk : split c rest
+    where (chunk, rest) = break (==c) s
+
+main :: IO ()
+main = do
+    a1
+
+
+
+data Signal = Low  { from :: String }
+            | High { from :: String }
+            deriving Show
+
+isHigh :: Signal -> Bool
+isHigh (High _) = True
+isHigh _ = False
+
+data ModuleType = Broadcast { name :: String, outs :: [String] }
+                | FlipFlop  { name :: String, outs :: [String] }
+                | Nand      { name :: String, outs :: [String] }
+                deriving Show
+
+data ModuleState = None
+                 | Bit Bool
+                 | Bits [(String,Bool)]
+                 deriving Show
+
+type Module = (ModuleType,ModuleState)
+type Counter = State (Int,Int)
+
+countSignal :: Signal -> Counter ()
+countSignal (Low _)  = modify (\(l,h) -> (l+1,h))
+countSignal (High _) = modify (\(l,h) -> (l,h+1))
+
+getInput :: IO [ModuleType]
+getInput = do
+    content <- readFile "y2023/inputs/d20"
+    return $ Broadcast "rx" [] : (parse <$> lines content)
+    where
+        parse (s:str) | s == '%'  = FlipFlop     name  nodes
+                      | s == '&'  = Nand         name  nodes
+                      | otherwise = Broadcast (s:name) nodes
+            where
+                name  = takeWhile (/=' ') str
+                nodes = tail <$> split ',' (drop 2 $ dropWhile (/='-') str)
+
+createModules :: [ModuleType] -> [Module]
+createModules types = create <$> types
+    where
+        create t@(Broadcast _ _) = (t, None)
+        create t@(FlipFlop _ _)  = (t, Bit False)
+        create t@(Nand n _)      = (t, Bits $ (,False) . name <$> filter ((n `elem`) . outs) types)
+
+findModule :: String -> [Module] -> (Module, [Module])
+findModule n [] = error $ "Could not find module: " ++ n
+findModule n (m:ms) | name (fst m) == n = (m,ms)
+                    | otherwise         = let (y,ys) = findModule n ms in (y,m:ys)
+
+signal :: Signal -> Module -> Counter (Module,[(Signal,String)])
+signal s m@(Broadcast n to, _) = do
+    countSignal s
+    return (m, (Low n,) <$> to)
+
+signal s@(Low _) (t@(FlipFlop n to), Bit b) = do
+    countSignal s
+    let b' = not b
+    let m' = (t, Bit b')
+    let s' = if b' then High n else Low n
+    return (m', (s',) <$> to)
+signal s@(High _) m@(FlipFlop n _, _) = do
+    countSignal s
+    return (m, [])
+
+signal s (t@(Nand n to), Bits bs) = do
+    countSignal s
+    let bs' = (from s, isHigh s) : filter ((/= from s) . fst) bs
+    let m'  = (t, Bits bs')
+    let s'  = if all snd bs' then Low n else High n
+    return (m', (s',) <$> to)
+
+propagate :: [Module] -> [(Signal,String)] -> Counter [Module]
+propagate modules [] = return modules
+propagate modules ((sig,name):ss) = do
+    let (m,ms) = findModule name modules
+    (m',response) <- signal sig m
+    propagate (m':ms) (ss ++ response)
+
+a1 :: IO ()
+a1 = do
+    modules <- createModules <$> getInput
+    let presses = replicate 1000 [(Low "button", "broadcaster")]
+    print $ uncurry (*) $ execState (foldM propagate modules presses) (0,0)
