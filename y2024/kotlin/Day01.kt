3c5ac1e9604c46cc7b415dcb3def1ae29f985507
Verzaukeks <verzaukeks@gmail.com>
2024-12-01 18:30:59 +0100

    Day: 01 (year: 2024)

---
+++ y2024/kotlin/Day01.kt
@@ -0,0 +1,37 @@
+package y2024
+
+import general.Day
+import kotlin.math.absoluteValue
+
+fun main() {
+//    Day01.a1()
+    Day01.a2()
+}
+
+object Day01 : Day() {
+    override val name = ""
+
+    override fun a1() {
+        val bs = ArrayList<Int>()
+        val cs = ArrayList<Int>()
+        INPUT.readText()
+            .split("\\W+".toRegex())
+            .map(String::toInt)
+            .chunked(2)
+            .forEach { (b, c) -> bs += b ; cs += c }
+        bs.sort()
+        cs.sort()
+        println(bs.zip(cs).sumOf { (a, b) -> (a - b).absoluteValue })
+    }
+
+    override fun a2() {
+        val bs = LongArray(1_000_000)
+        val cs = LongArray(1_000_000)
+        INPUT.readText()
+            .split("\\W+".toRegex())
+            .map(String::toInt)
+            .chunked(2)
+            .forEach { (b, c) -> bs[b]++ ; cs[c]++ }
+        println(bs.indices.sumOf { it * bs[it] * cs[it] })
+    }
+}
