0b805b555d4e42fdee589036a5b47469e6b720a5
Verzaukeks <verzaukeks@gmail.com>
2024-12-03 22:59:58 +0100

    Day: 02 (year: 2024) [2.star nasm]

--- y2024/nasm/day02.asm
+++ y2024/nasm/day02.asm
@@ -6,23 +6,30 @@ section .data
 
 section .bss
     ns:     resd 10000
+    ts:     resd 10
 
 section .text
     global main
     extern printf
+    default rel
 
 
 main:
-    push rbp
+    sub rsp, 40
 
     call parse
 
     call a1
-    lea rcx, [rel FMT]
+    lea rcx, [FMT]
     mov rdx, rax
     call printf
-    
-    pop rbp
+
+    call a2
+    lea rcx, [FMT]
+    mov rdx, rax
+    call printf
+
+    add rsp, 40
     xor rax, rax
     ret
 
@@ -116,4 +123,94 @@ a1_next:
     cmp ebx, -1
     jne a1_inc_ini
 a1_end:
+    ret
+
+
+; r8: result
+; r9: nums pointer
+; r10: current nums pointer
+; r11: current temps pointer
+; r12: skip index (pointer)
+a2:
+    xor r8, r8
+    lea r9, [ns]
+a2_ini:
+    mov r12, r9
+    sub r12, 4
+a2_prepare_ini:
+    mov r10, r9
+    lea r11, [ts]
+a2_prepare:
+    mov eax, [r10]
+    add r10, 4
+    cmp eax, 0
+    jz a2_test
+    mov rbx, r12
+    add rbx, 4
+    cmp rbx, r10
+    je a2_prepare
+    mov [r11], eax
+    add r11, 4
+    jmp a2_prepare
+a2_test:
+    mov dword [r11], 0
+    call safe_ts
+    cmp rax, 0
+    jz a2_skip_next
+    add r8, 1
+    jmp a2_next
+a2_skip_next:
+    add r12, 4
+    mov eax, [r12]
+    cmp eax, 0
+    jnz a2_prepare_ini
+a2_next:
+    mov eax, [r9]
+    add r9, 4
+    cmp rax, 0
+    jnz a2_next
+    mov eax, [r9]
+    cmp eax, -1
+    jne a2_ini
+a2_end:
+    mov rax, r8
+    ret
+
+
+; rax: result
+; rbx: temps pointer
+; rcx: current num
+; rdx: next num
+safe_ts:
+    xor rax, rax
+    lea rbx, [ts]
+safe_ts_inc:
+    mov ecx, [rbx]
+    mov edx, [rbx + 4]
+    cmp rdx, 0
+    jz safe_ts_success
+    sub rdx, rcx
+    cmp rdx, 1
+    jl safe_ts_dec_ini
+    cmp rdx, 3
+    jg safe_ts_dec_ini
+    add rbx, 4
+    jmp safe_ts_inc
+safe_ts_dec_ini:
+    lea rbx, [ts]
+safe_ts_dec:
+    mov ecx, [rbx]
+    mov edx, [rbx + 4]
+    cmp rdx, 0
+    jz safe_ts_success
+    sub rcx, rdx
+    cmp rcx, 1
+    jl safe_ts_fail
+    cmp rcx, 3
+    jg safe_ts_fail
+    add rbx, 4
+    jmp safe_ts_dec
+safe_ts_success:
+    mov rax, 1
+safe_ts_fail:
     ret
